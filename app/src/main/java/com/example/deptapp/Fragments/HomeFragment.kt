8cad776385743d4e96a4ce358fdf5bfe935c8621
Chandan Singha <chandan094.hitit2020@gmail.com>
2023-02-04 03:58:02 +0530

    Slider Images Add

--- app/src/main/java/com/example/deptapp/Fragments/HomeFragment.kt
+++ app/src/main/java/com/example/deptapp/Fragments/HomeFragment.kt
@@ -1,10 +1,16 @@
 package com.example.deptapp.Fragments
 
 import android.os.Bundle
+import android.view.Gravity
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.view.contains
+import com.denzcoskun.imageslider.ImageSlider
+import com.denzcoskun.imageslider.constants.ScaleTypes
+import com.denzcoskun.imageslider.models.SlideModel
 import com.example.deptapp.R
 import com.example.deptapp.databinding.FragmentHomeBinding
 import kotlinx.coroutines.flow.combine
@@ -21,6 +27,7 @@ private const val ARG_PARAM2 = "param2"
  */
 class HomeFragment : Fragment() {
     lateinit var binding : FragmentHomeBinding
+    lateinit var imageList: ArrayList<SlideModel>
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -29,7 +36,15 @@ class HomeFragment : Fragment() {
         binding = FragmentHomeBinding.inflate(layoutInflater, container, false)
 
         binding.marqueeText.isSelected = true
+        imageList= ArrayList()
 
+        imageList.add(SlideModel(R.drawable.four,"Haldia Institute Of Technology"))
+        imageList.add(SlideModel(R.drawable.three,"ACM Hack Track Workshop"))
+        imageList.add(SlideModel(R.drawable.one,"ACM Members"))
+        imageList.add(SlideModel(R.drawable.four,"Prize Distribution"))
+        imageList.add(SlideModel(R.drawable.two,"Workshop"))
+        binding.imageSlider.textAlignment=View.TEXT_ALIGNMENT_CENTER
+        binding.imageSlider.setImageList(imageList,ScaleTypes.CENTER_CROP)
         return binding.root
     }
 
