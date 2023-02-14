c6cc5af2f90cb0a796f9f71e8649621446ae0d5a
Chandan Singha <chandan094.hitit2020@gmail.com>
2023-02-15 03:59:05 +0530

    Home Page 80% done

--- app/src/main/java/com/example/deptapp/Fragments/HomeFragment.kt
+++ app/src/main/java/com/example/deptapp/Fragments/HomeFragment.kt
@@ -6,8 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.core.view.contains
+import com.bumptech.glide.Glide
 import com.denzcoskun.imageslider.ImageSlider
 import com.denzcoskun.imageslider.constants.ScaleTypes
 import com.denzcoskun.imageslider.models.SlideModel
@@ -35,17 +37,28 @@ class HomeFragment : Fragment() {
 
         binding = FragmentHomeBinding.inflate(layoutInflater, container, false)
 
+        setUp()
+
         binding.marqueeText.isSelected = true
         imageList= ArrayList()
 
-        imageList.add(SlideModel(R.drawable.four,"Haldia Institute Of Technology"))
-        imageList.add(SlideModel(R.drawable.three,"ACM Hack Track Workshop"))
-        imageList.add(SlideModel(R.drawable.one,"ACM Members"))
-        imageList.add(SlideModel(R.drawable.four,"Prize Distribution"))
-        imageList.add(SlideModel(R.drawable.two,"Workshop"))
-        binding.imageSlider.textAlignment=View.TEXT_ALIGNMENT_CENTER
+        imageList.add(SlideModel("https://www.admissionfever.com/Media/clgimg/gallery/2092_img7281787176718769.png","Haldia Institute Of Technology"))
+        imageList.add(SlideModel("https://firebasestorage.googleapis.com/v0/b/social-media-2-0.appspot.com/o/images%2Fthree-min.JPG?alt=media&token=26feb110-4f7b-45eb-8ec2-80769de26f44","ACM Hack Track Workshop"))
+        imageList.add(SlideModel("https://firebasestorage.googleapis.com/v0/b/social-media-2-0.appspot.com/o/images%2Ffour-min.JPG?alt=media&token=ad0aace8-0686-4ef8-8852-6e3fb4867c72","ACM Members"))
+        imageList.add(SlideModel("https://firebasestorage.googleapis.com/v0/b/social-media-2-0.appspot.com/o/images%2Fone-min.JPG?alt=media&token=c5e88727-c810-42d9-886f-9953073ec654","Prize Distribution"))
+        imageList.add(SlideModel("https://firebasestorage.googleapis.com/v0/b/social-media-2-0.appspot.com/o/images%2Ftwo.JPG?alt=media&token=059ccf95-ede1-4787-a833-caaec8e56a91","Workshop"))
         binding.imageSlider.setImageList(imageList,ScaleTypes.CENTER_CROP)
         return binding.root
+    }
+
+    private fun setUp(){
+        Glide.with(this).load("https://www.admissionfever.com/Media/clgimg/gallery/2092_img7281787176718769.png").into(binding.imgEvent1)
+        Glide.with(this).load("https://www.admissionfever.com/Media/clgimg/gallery/2092_img7281787176718769.png").into(binding.imgEvent2)
+        Glide.with(this).load("https://www.admissionfever.com/Media/clgimg/gallery/2092_img7281787176718769.png").into(binding.imgEvent3)
+        Glide.with(this).load("https://www.admissionfever.com/Media/clgimg/gallery/2092_img7281787176718769.png").into(binding.imgEvent4)
+        Glide.with(this).load("https://www.admissionfever.com/Media/clgimg/gallery/2092_img7281787176718769.png").into(binding.imgEvent5)
+
+
     }
 
 }
