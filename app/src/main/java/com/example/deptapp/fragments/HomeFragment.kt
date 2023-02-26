d8a794c27c21aa821240cdaffa51eeec7372bc5b
Anant <anantkr0202@gmail.com>
2023-02-26 22:53:05 +0530

    Shimmer Batch Fragment Done

--- app/src/main/java/com/example/deptapp/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/deptapp/fragments/HomeFragment.kt
@@ -170,7 +170,7 @@ class HomeFragment : Fragment(), EventItem2Clicked {
                 for(i in 0 until noticeJsonArray.length()){
                     val noticeJsonObject = noticeJsonArray.getJSONObject(i)
                     val notice = NoticeData(
-                        noticeJsonObject.getString("title"),
+                        noticeJsonObject.getString("batch"),
                         noticeJsonObject.getString("pdfurl"),
                         noticeJsonObject.getString("date"),
                         noticeJsonObject.getString("pdfid")
